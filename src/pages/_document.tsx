10651b5b0a8a211fb7c2ef227b378f37997f4e7b
MakanaMakesStuff <makrigamedeveloping@gmail.com>
2023-03-04 20:12:43 -0500

    pushing crop a lot code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <body>
         <Main />
         <NextScript />
